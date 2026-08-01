afc0b355190e47e0df22e9826c648c0e1d3f49e1
Midhun Sudhakaran <midhun.sudhakaran@qburst.com>
2026-08-01 20:33:07 +0530

    Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 plugins: [react()],
-base: '/expense-tracker/',
-})
+base: process.env.NODE_ENV === "production"
+? "/expense-tracker/"
+: "/",
+});
