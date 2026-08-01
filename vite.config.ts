2c8d6593b501f0791df36522960d559bdbda9710
Midhun Sudhakaran <midhun.sudhakaran@qburst.com>
2026-08-01 21:09:13 +0530

    New updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-plugins: [react()],
-base: process.env.NODE_ENV === "production"
-? "/expense-tracker/"
-: "/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Expense Tracker",
+        short_name: "Expense",
+        description: "Personal Expense Tracker",
+        theme_color: "#1976d2",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/expense-tracker/",
+        scope: "/expense-tracker/",
+
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/expense-tracker/",
 });
